efd458e9ef1992f3c225c2816b4d3211dded6665
fabiojmf <fabiojmf@gmail.com>
2020-06-04 15:42:49 -0300

    atualizacoes

---
+++ media_pontos/media-import-datas.R
@@ -0,0 +1,27 @@
+library(xml2)
+library(rvest)
+library(stringr)
+
+URL <-
+  'https://br.investing.com/currencies/usd-brl-bmf-futures-historical-data'
+
+lista.tabela <-
+  read_html(URL) %>% html_nodes("#curr_table") %>% html_table(fill = T)
+df.valores <- lista.tabela[[1]]
+
+df.valores$Máxima <-
+  as.numeric(str_remove(df.valores$Máxima, "[,]"))
+df.valores$Mínima <-
+  as.numeric(str_remove(df.valores$Mínima, "[,]"))
+
+
+media.cinco.dias <-
+  round(mean((df.valores$Máxima[1:5] - df.valores$Mínima[1:5]) * 1000))
+media.dez.dias <-
+  round(mean((df.valores$Máxima[1:10] - df.valores$Mínima[1:10]) * 1000))
+media.quinze.dias <-
+  round(mean((df.valores$Máxima[1:15] - df.valores$Mínima[1:15]) * 1000))
+media.vinte.dias <-
+  round(mean((df.valores$Máxima[2:22] - df.valores$Mínima[2:22]) * 1000))
+media.trinta.dias <-
+  round(mean((df.valores$Máxima[1:nrow(df.valores)] - df.valores$Mínima[1:nrow(df.valores)]) * 1000))
