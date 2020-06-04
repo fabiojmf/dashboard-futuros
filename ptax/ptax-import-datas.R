aa53d88986d09039514243e989ab76cbba7d39a4
fabiojmf <fabiojmf@gmail.com>
2020-06-04 15:52:14 -0300

    atualizacoes

--- ptax/ptax-import-datas.R
+++ ptax/ptax-import-datas.R
@@ -1,30 +1,4 @@
-library(xml2)
-library(rvest)
-library(stringr)
-
 source('util/util-dates.R')
-
-# ptax.pregao.atual < function() {
-#   data.pesquisa.ptax <- get.dia.hoje()
-#   URL <-
-#     str_glue(
-#       'https://ptax.bcb.gov.br/ptax_internet/consultaBoletim.do?method=consultarBoletim&RadOpcao=3&DATAINI=',
-#       data.pesquisa.ptax,
-#       "&DATAFIM=&ChkMoeda=61"
-#     )
-#   
-#   lista.tabela <-
-#     read_html(URL) %>% html_nodes("table") %>% html_table(fill = T)
-#   df.aberturas <- lista.tabela[[1]]
-#   
-#   if(str_count(df.aberturas[3, 4]) == 6){
-#     aux.df <- data.frame("Valores" = as.numeric(sub(",", ".", df.aberturas[3, 4])))
-#     if(str_count(df.aberturas[4, 4]) == 6){
-#       aux.df[nrow(aux.df)+1,] = as.numeric(sub(",", ".", df.aberturas[4, 4]))
-#     }
-#   }
-# }
-
 
 data.pesquisa.ptax <- get.dia.anterior()
 URL <-
